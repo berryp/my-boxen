1b01014880301c7437ffe55d3cbe1c506eca52fa
Berry Phillips <berryphillips@gmail.com>
2013-02-18 22:16:31 -0800

    Initial customizations.

---
+++ modules/people/manifests/berryp.pp
@@ -0,0 +1,25 @@
+class people::berryp {
+
+  notify { 'class people::berryp declared': }
+
+  include people::glarizza::applications
+
+  ##################################
+  ## Facter, Puppet, and Envpuppet##
+  ##################################
+
+  repository { "${::boxen_srcdir}/puppet":
+    source => 'puppetlabs/puppet',
+  }
+
+  repository { "${::boxen_srcdir}/facter":
+    source => 'puppetlabs/facter',
+  }
+
+  file { '/bin/envpuppet':
+    ensure  => link,
+    mode    => '0755',
+    target  => "${::boxen_srcdir}/puppet/ext/envpuppet",
+    require => Repository["${::boxen_srcdir}/puppet"],
+  }
+}
---
+++ modules/people/manifests/berryp/applications.pp
@@ -0,0 +1,3 @@
+class people::berryp::applications {
+  include chrome::dev
+}
