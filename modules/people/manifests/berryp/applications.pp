8a7dc69a6b5b4afb6764b3a26a8ceee1a15b637a
Berry Phillips <berryphillips@gmail.com>
2013-02-19 15:21:18 -0800

    Adding cache

--- modules/people/manifests/berryp/applications.pp
+++ modules/people/manifests/berryp/applications.pp
@@ -1,7 +1,7 @@
 class people::berryp::applications {
-  include chrome::dev
   include mongodb
   include python
+  include chrome::dev
   include dropbox
   include sublime_text_2
   include onepassword
